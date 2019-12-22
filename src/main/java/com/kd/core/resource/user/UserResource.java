71676ca1743880b9b53750bbbc0a59f960ba9b0c
gsb0505 <15168268827@139.com>
2019-12-22 14:56:30 +0800

    wxlogin

--- src/main/java/com/kd/core/resource/user/UserResource.java
+++ src/main/java/com/kd/core/resource/user/UserResource.java
@@ -13,12 +13,18 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 
 
+
+
+
 import com.google.gson.Gson;
+import com.kd.common.unit.util.EncryptUtils;
 import com.kd.core.entity.PageCount;
 import com.kd.core.entity.Role;
 import com.kd.core.entity.UserInfo;
@@ -38,9 +44,11 @@ import com.kd.core.util.CoreUtil;
  */
 @Path("/user")
 public class UserResource {
+	private static Logger log = LoggerFactory.getLogger(UserResource.class);
+	
 	@Autowired
 	private UserService userService;
-	
+
 	
 	public UserService getUserService() {
 		return userService;
@@ -145,7 +153,17 @@ public class UserResource {
 		return userService.updateErr(userInfo);
 	}
 	
-	
+	@GET
+	@Path("wxLogin")
+	public String wxLogin(@QueryParam("user") String user){
+		UserInfo u = new Gson().fromJson(user, UserInfo.class);
+		
+		EncryptUtils loginE = new EncryptUtils(u.getUserId(), "MD5");
+		String inPws = loginE.encode(u.getLoginPSW());
+		u.setLoginPSW(inPws);
+		log.debug(new Gson().toJson(userService.validUser(u)));
+		return 	new Gson().toJson(userService.validUser(u));
+	}
 	
 	
 	
